270ac41b4dd6108623cc31ec1debab18c877d93d
Brian Jensen <jensen_305@yahoo.com>
2021-03-31 22:25:10 -0700

    Initial Connection to a game working

--- server.js
+++ server.js
@@ -15,37 +15,78 @@ function Server(position, id, status) {
 }
 
 const Status = { FULL: 1, OPEN: 2, LATER: 3 };
+function CheckMatchMaking() {
+    if (MatchMaking.length < 2)
+        return;
 
+    let RequestGame = {
+        BlueTeam: MatchMaking.pop(),
+        RedTeam: MatchMaking.pop(),
+        RoomName: nanoID.nanoid()
+    }
+    let sid = -1;
+    for (var n in GameServers) {
+        if (GameServers[n].status == Status.OPEN) {
+            sid = n;
+            break;
+        }
+    }
+    //Have game
+    if (sid != -1) {
+        console.log(GameServers[sid].id);
+        io.to(GameServers[sid].id).emit('RequestGame', { RequestGame: RequestGame });
+        console.log("Requesting Game");
+    } else {
+        //No game available
+        io.to(RequestGame.BlueTeam).to(RequestGame.RedTeam).emit('ServerHostError');
+    }
+}
 StartServer();
 async function StartServer() {
-    let MatchMakingInterval = setInterval(() => {
-        if (MatchMaking.length < 2)
-            return;
-        console.log("Game needed");
-        let RequestGame = {
-            BlueTeam: MatchMaking.pop(),
-            RedTeam: MatchMaking.pop(),
-            RoomName: nanoID.nanoid()
-        }
-        let sid = -1;
-        for (var n in GameServers) {
-            if (GameServers[n].status == Status.OPEN) {
-                sid = n;
-                break;
-            }
-        }
-        //Have game
-        if (sid != -1) {
-            console.log(GameServers[sid].id);
-            io.to(GameServers[sid].id).emit('RequestGame', {RequestGame: RequestGame});
-            console.log("Requesting Game");
-        } else {
-            //No game available
-            socket.emit('ServerHostError');
-        }
-    }, 1000);
+    //let MatchMakingInterval = setInterval(() => {
+    //    CheckMatchMaking();
+    //}, 1000);
     io.on('connection', Connect);
     console.log("Started Server");
+}
+
+function SetGameServerFull(id) {
+    for(let n in GameServers) {
+        if (GameServers[n].id == id) {
+            GameServers[n].status = FULL;
+            break;
+        }
+    }
+}
+
+function SetGameServerOpen(id) {
+    for(let n in GameServers) {
+        if (GameServers[n].id == socket.id) {
+            GameServers[n].status = OPEN;
+            break;
+        }
+    }
+}
+
+function PutIntoMatchMaking(BlueTeam, RedTeam) {
+    if (io.sockets.connected[BlueTeam])
+        MatchMaking.unshift(BlueTeam);
+    if (io.sockets.connected[RedTeam])
+        MatchMaking.unshift(RedTeam);
+}
+
+function RegisterGameBetween(BlueTeam, RedTeam, RoomName) {
+    io.sockets.connected[BlueTeam].currentGame = RoomName;
+    io.sockets.connected[BlueTeam].currentEnemy = RedTeam;
+    io.sockets.connected[BlueTeam].playAgain = false;
+    io.sockets.connected[BlueTeam].responded = false;
+    io.sockets.connected[BlueTeam].join(RoomName);
+
+    io.sockets.connected[RedTeam].currentGame = RoomName;
+    io.sockets.connected[RedTeam].currentEnemy = BlueTeam;
+    io.sockets.connected[RedTeam].playAgain = false;
+    io.sockets.connected[RedTeam].responded = false;
+    io.sockets.connected[RedTeam].join(RoomName);
 }
 
 function Connect(socket) {
@@ -55,6 +96,7 @@ function Connect(socket) {
     let inGame = false;
     let lookingForGame = false;
     socket.on('RegisterGameServer', ({ APIKEY }) => {
+        console.log(APIKEY)
         if (APIKEY == GAMESERVERPASSWORD) {
             isGameServer = true;
             hasGameServer = true;
@@ -62,38 +104,52 @@ function Connect(socket) {
             //This assumes never disconnecting
             gs = new Server(GameServers.length, socket.id, Status.OPEN);
             GameServers.push(gs);
+
             console.log(`Game Server Registered. Game Server Count ${GameServerCount}`);
+
             socket.on('AskPlayAgain', ({ BlueTeam, RedTeam, RoomName }) => {
                 socket.broadcast.to(RoomName).emit('PlayAgain');
                 //At this point we will no longer know anything about this
             });
+
             socket.on('GameOver', ({ BlueTeam, RedTeam, RoomName }) => {
                 //We want the option to replay
                 io.sockets.clients(RoomName).forEach(() => s.leave(RoomName));
                 io.to(BlueTeam).to(RedTeam).emit('GameOver');
                 selection = [false, false, false, false, false, false];
             });
-            socket.on('GameRegistered', ({ BlueTeam, RedTeam, RoomName }) => {
-                //BlueTeam & RedTeam are socket.id of the players requesting the game
-                io.sockets.connected[BlueTeam].currentGame = RoomName;
-                io.sockets.connected[BlueTeam].currentEnemy = RedTeam;
-                io.sockets.connected[BlueTeam].playAgain = false;
-                io.sockets.connected[BlueTeam].responded = false;
-                io.sockets.connected[BlueTeam].join(RoomName);
-                io.sockets.connected[RedTeam].currentGame = RoomName;
-                io.sockets.connected[RedTeam].currentEnemy = BlueTeam;
-                io.sockets.connected[RedTeam].playAgain = false;
-                io.sockets.connected[RedTeam].responded = false;
-                io.sockets.connected[RedTeam].join(RoomName);
-                socket.join(RoomName);
-                io.to(BlueTeam).to(RedTeam).emit('InGame');
+            
+            socket.on('OpenServer', () => {
+                SetGameServerOpen(socket.id);
+                console.log("GameServer requesting opening");
             });
-            socket.join(socket.id);
+
+            //Called when GameServer is Full Also called during closing of everyone passed to us
+            socket.on('GameClosed', ({ BlueTeam, RedTeam, RoomName }) => {
+                if (RedTeam && BlueTeam)
+                    PutIntoMatchMaking(BlueTeam, RedTeam);
+                SetGameServerFull(socket.id);
+                console.log("GameServer requesting closure.");
+            });
+
+            socket.on('GameRegistered', ({ BlueTeam, RedTeam, RoomName }) => {
+                if (io.sockets.connected[BlueTeam] && io.sockets.connected[RedTeam]) {
+                    RegisterGameBetween(BlueTeam, RedTeam, RoomName);
+                    socket.join(RoomName);
+                    io.to(BlueTeam).to(RedTeam).emit('InGame');
+                } else {
+                    PutIntoMatchMaking(BlueTeam, RedTeam);
+                    socket.emit('CancelGame', ({RoomName}));
+                    CheckMatchMaking();
+                }
+            });
+
+            socket.emit('RegisteredAsGameServer');
         } else {
             console.log(`Game Server Attempt Incorrect password`);
             socket.disconnect(true);
+            return;
         }
-        socket.emit('RegisteredAsGameServer')
     });
 
     function isString(value) {
@@ -119,7 +175,9 @@ function Connect(socket) {
             lookingForGame = true;
 
             MatchMaking.push(socket.id);
-            console.log("added player to match making")
+            console.log(`${username} added to Matchmaking`);
+            CheckMatchMaking();
+            
             //Gameserver finds an open game and sends us back the ID and SocketID
             //From here no longer in this section of code. 
             //io.in(socketID).Emit('InGame');
@@ -128,6 +186,7 @@ function Connect(socket) {
         });
 
         socket.on('PlayAgain', ({ result }) => {
+            //Still needs a lot of work
             if (!inGame)
                 return;
             //socket.currentGame
@@ -136,8 +195,15 @@ function Connect(socket) {
             socket.responded = true;
             let socket = io.sockets.connected[socket.currentEnemy];
             if (socket.responded == true && socket.playAgain != true) {
-                let gameServerSocket = GameServers[socket.currentGame];
-                io.sockets.connected[gameServerSocket].emit('EndGame', { Game: socket.currentGame });
+               
+                for(let gs in GameServers) {
+                    if (GameServers[gs].id == socket.currentGame) {
+                        let gameServerSocket = GameServers[gs];
+                        io.sockets.connected[gameServerSocket.id].emit('EndGame', { Game: socket.currentGame });
+                        break;
+                    }
+                }
+                
                 //Both players want to play again
                 //Nope code
             }
@@ -154,7 +220,7 @@ function Connect(socket) {
             let newPoint = { GameID: 3, Selection: selected, Point: Point };
             //
         });
- 
+
         socket.on('Select', ({ Units }) => {
             if (!Array.isArray(Units))
                 return;
